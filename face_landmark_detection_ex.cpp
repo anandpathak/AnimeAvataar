1dcef12dcc34e7abbad6f9470d1a550b9a3063de
anand pathak <anandpathak69@gmail.com>
2016-05-08 17:30:02 +0530

    remove data files and unused files

--- face_landmark_detection_ex.cpp
+++ face_landmark_detection_ex.cpp
@@ -1,43 +1,3 @@
-// The contents of this file are in the public domain. See LICENSE_FOR_EXAMPLE_PROGRAMS.txt
-/*
-
-    This example program shows how to find frontal human faces in an image and
-    estimate their pose.  The pose takes the form of 68 landmarks.  These are
-    points on the face such as the corners of the mouth, along the eyebrows, on
-    the eyes, and so forth.  
-    
-
-
-    This face detector is made using the classic Histogram of Oriented
-    Gradients (HOG) feature combined with a linear classifier, an image pyramid,
-    and sliding window detection scheme.  The pose estimator was created by
-    using dlib's implementation of the paper:
-        One Millisecond Face Alignment with an Ensemble of Regression Trees by
-        Vahid Kazemi and Josephine Sullivan, CVPR 2014
-    and was trained on the iBUG 300-W face landmark dataset.  
-
-    Also, note that you can train your own models using dlib's machine learning
-    tools.  See train_shape_predictor_ex.cpp to see an example.
-
-    
-
-
-    Finally, note that the face detector is fastest when compiled with at least
-    SSE2 instructions enabled.  So if you are using a PC with an Intel or AMD
-    chip then you should enable at least SSE2 instructions.  If you are using
-    cmake to compile this program you can enable them by using one of the
-    following commands when you create the build project:
-        cmake path_to_dlib_root/examples -DUSE_SSE2_INSTRUCTIONS=ON
-        cmake path_to_dlib_root/examples -DUSE_SSE4_INSTRUCTIONS=ON
-        cmake path_to_dlib_root/examples -DUSE_AVX_INSTRUCTIONS=ON
-    This will set the appropriate compiler options for GCC, clang, Visual
-    Studio, or the Intel compiler.  If you are using another compiler then you
-    need to consult your compiler's manual to determine how to enable these
-    instructions.  Note that AVX is the fastest but requires a CPU from at least
-    2011.  SSE4 is the next fastest and is supported by most current machines.  
-*/
-
-
 #include "./dlib/image_processing/frontal_face_detector.h"
 #include "./dlib/image_processing/render_face_detections.h"
 #include "./dlib/image_processing.h"
@@ -54,10 +14,6 @@ int main(int argc, char** argv)
 {  
     try
     {
-        // This example takes in a shape model file and then a list of images to
-        // process.  We will take these filenames in as command line arguments.
-        // Dlib comes with example images in the examples/faces folder so give
-        // those as arguments to this program.
         if (argc == 1)
         {
             cout << "Call this program like this:" << endl;
@@ -79,6 +35,7 @@ int main(int argc, char** argv)
 
 
         image_window win, win_faces;
+        array2d<rgb_pixel> img2;
         // Loop over all the images provided on the command line.
         for (int i = 2; i < argc; ++i)
         {
@@ -115,7 +72,9 @@ int main(int argc, char** argv)
 
             // Now let's view our face poses on the screen.
             win.clear_overlay();
-            win.set_image(img);
+            win.set_image(img2);
+            //win.set_image(img);
+
             win.add_overlay(render_face_detections(shapes));
 
             // We can also extract copies of each face that are cropped, rotated upright,
