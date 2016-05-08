f5f92163d79f8a3c424d7127c5f368861adeea45
anand pathak <anandpathak69@gmail.com>
2016-05-08 18:29:09 +0530

    improved file structure

--- src/face_landmark_detection_ex.cpp
+++ src/face_landmark_detection_ex.cpp
@@ -1,8 +1,8 @@
-#include "./dlib/image_processing/frontal_face_detector.h"
-#include "./dlib/image_processing/render_face_detections.h"
-#include "./dlib/image_processing.h"
-#include "./dlib/gui_widgets.h"
-#include "./dlib/image_io.h"
+#include "../dlib/image_processing/frontal_face_detector.h"
+#include "../dlib/image_processing/render_face_detections.h"
+#include "../dlib/image_processing.h"
+#include "../dlib/gui_widgets.h"
+#include "../dlib/image_io.h"
 #include <iostream>
 
 using namespace dlib;
